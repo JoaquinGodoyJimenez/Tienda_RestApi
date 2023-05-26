73da9dfb55e057b41e24a2eb4af819dc97a520de
JoaquinGodoyJimenez <19030394@itcelaya.edu.mx>
2023-05-25 23:27:00 -0600

    user controller correction

--- src/controllers/usuario.controller.js
+++ src/controllers/usuario.controller.js
@@ -1,12 +1,11 @@
 const { Pool } = require('pg');
 
 const pool = new  Pool({
-    host: 'localhost',
-    user: 'postgres',
-    password: 'Pikmin45',
-    database: 'tienda',
-    port: '5432',
-})
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 const getUsers = async (req, res) => {
     const response = await pool.query('SELECT * FROM usuario');
